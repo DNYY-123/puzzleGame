c713cd629934fbb4ab9490b1f760cc0ccead6c77
DNYY-123 <2509184782@qq.com>
2022-12-28 00:28:06 +0800

    ui界面

---
+++ src/com/itheima/ui/GameJFrame.java
@@ -0,0 +1,21 @@
+package com.itheima.ui;
+
+import javax.swing.*;
+
+public class GameJFrame extends JFrame {
+
+    public GameJFrame() {
+        // 设置窗口的大小
+        this.setSize(603, 680);
+        // 设置窗口的名称
+        this.setTitle("拼图小游戏 V1.0");
+        // 设置窗口总是显示在屏幕最顶层
+        this.setAlwaysOnTop(true);
+        // 设置窗口居中
+        this.setLocationRelativeTo(null);
+        // 设置窗口关闭方式 代表关闭一个窗口程序会停止
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        // 设置窗口的显示
+        this.setVisible(true);
+    }
+}
